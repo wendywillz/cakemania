1b780f69bf992f0cb416faa6cb5f3168bb556ac4
Nkachukwu-Nwobi <nkachukwu.nwobi@decagon.dev>
2023-12-24 23:58:38 +0100

    login-error

--- src/controller/admincontroller.ts
+++ src/controller/admincontroller.ts
@@ -14,23 +14,18 @@ export async function getAdminDashboard(req: AuthRequest, res: Response){
       // Assuming you have user information available in req.user after authentication
       const userID = req.user?.userID;
   
-      const user = await Users.findAll({
-        where: { userID: userID }, // Adjust the column name according to your database schema
-      });
-      
-
-      const regularUsers = await Users.findAll({
-        where: { isAdmin: false }, // Adjust the column name according to your database schema
+      const admin = await Users.findAll({
+        where: { userID: userID, isAdmin: true }, // Adjust the column name according to your database schema
       });
 
 
       // Fetch products by user ID
-      const userProducts = await Cakes.findAll({
+      const adminCakes = await Cakes.findAll({
         where: { userID: userID }, // Adjust the column name according to your database schema
       });
   
       // Render the products page with the fetched data
-      res.render('products-page', { user, userProducts });
+      res.render('admin-index', { admin, adminCakes});
     } catch (error) {
       // Handle errors appropriately
       res.status(500).send('Internal Server Error');
