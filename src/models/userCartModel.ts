3fa4464377ac3fe78cf8b46f225ccb03a5df0012
Nkachukwu-Nwobi <nkachukwu.nwobi@decagon.dev>
2024-01-21 17:29:50 +0100

    toggle update

--- src/models/userCartModel.ts
+++ src/models/userCartModel.ts
@@ -1,56 +1,56 @@
-import {Sequelize, Model, DataTypes} from "sequelize" //I did not install any package
-import sequelize from "../database.config" 
-import Users from "./usermodel";
-import Cart from "./cartmodel";
-import Orders from "./ordermodel";
-import { v4 as uuidv4 } from 'uuid';
+// import {Sequelize, Model, DataTypes} from "sequelize" //I did not install any package
+// import sequelize from "../database.config" 
+// import Users from "./usermodel";
+// import Cart from "./cartmodel";
+// import Orders from "./ordermodel";
+// import { v4 as uuidv4 } from 'uuid';
 
-//could change the path to the config file
-// import Items from "./itemModel"
-// import Users  from "./usermodel"
-// import  Cakes  from "./cakemodel"
+// //could change the path to the config file
+// // import Items from "./itemModel"
+// // import Users  from "./usermodel"
+// // import  Cakes  from "./cakemodel"
 
-interface UserCartAtrributes{
-    orderID: string,
-    userID: string,
-    cartID: string,
-}
+// interface UserCartAtrributes{
+//     orderID: string,
+//     userID: string,
+//     cartID: string,
+// }
 
-export class UserCart extends Model <UserCartAtrributes> {}
-
-
-//Ive included the LGA and State. But I need to ask: if we're already asking for the address, doesn't that make requesting those, redundant?
-
-UserCart.init({
-    orderID:{
-        type: DataTypes.STRING,
-        references: {
-            model: Orders,
-            key: 'orderID'
-        },
-    },
-    userID: {
-        type: DataTypes.STRING,
-        references: {
-            model: Users,
-            key: 'userID'
-        },
-    },
-    cartID :{
-        type: DataTypes.STRING,
-        references: {
-            model: Cart,
-            key: 'cartID'
-        },
-    },
-
-}, {
-    sequelize,
-    modelName: "UserCart"
-})
+// export class UserCart extends Model <UserCartAtrributes> {}
 
 
+// //Ive included the LGA and State. But I need to ask: if we're already asking for the address, doesn't that make requesting those, redundant?
 
-// UserCart === sequelize.models.UserCart
+// UserCart.init({
+//     orderID:{
+//         type: DataTypes.STRING,
+//         references: {
+//             model: Orders,
+//             key: 'orderID'
+//         },
+//     },
+//     userID: {
+//         type: DataTypes.STRING,
+//         references: {
+//             model: Users,
+//             key: 'userID'
+//         },
+//     },
+//     cartID :{
+//         type: DataTypes.STRING,
+//         references: {
+//             model: Cart,
+//             key: 'cartID'
+//         },
+//     },
 
- export default UserCart
+// }, {
+//     sequelize,
+//     modelName: "UserCart"
+// })
+
+
+
+// // UserCart === sequelize.models.UserCart
+
+//  export default UserCart
